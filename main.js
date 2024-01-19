f80f76c1136a481c6f5018704b3449a4599eb2ad
lucasmontel <aimpostor6@gmail.com>
2024-01-19 11:03:24 -0300

    #

--- main.js
+++ main.js
@@ -1,58 +1,77 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const routes = require("./routes/router");
+const router = express.Router();
 const bodyParser = require("body-parser");
-const { Sequelize } = require("sequelize");
+const { Sequelize, DataTypes } = require("sequelize");
 
-//Configurando Sequelize para se conectar com MySQL
+// Configurando Sequelize para se conectar com MySQL
 const db = new Sequelize("loginmysql", "root", "jesus1981", {
-    host: "localhost",
-    dialect: "mysql"
-})
-
-//Tentando conexão
-db.authenticate().then(() => {
-    console.log("Conectado ao MySQL!");
-}).catch((err) => {
-    console.log("Não conectado ao MySQL:", err);
+  host: "localhost",
+  dialect: "mysql",
 });
 
-//Modelo de tabela
+// Modelo de tabela
 const User = db.define("user", {
-    email: {
-        type: Sequelize.STRING,
-        required: true
-    },
-    password: {
-        type: Sequelize.TEXT,
-        required: true
-    }
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
 });
-module.exports = User;
 
-//Body Parser
-//Dizemos que o Body Parser pode receber qualquer tipo de dados, não só dados específicos
+// Sincronizando o sequelize com o banco de dados;
+db.authenticate()
+  .then(() => {
+    console.log("Modelo sincronizado com o banco de dados!");
+  })
+  .catch((err) => {
+    console.error("Erro ao sincronizar modelo:", err);
+  });
+
+  
+  //Rotas
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+
+// Tratamento
+router.post("/tratando", async (req, res) => {
+  try {
+    const newUser = await User.create({
+      email: req.body.email,
+      password: req.body.password,
+    });
+    res.send("Usuário criado com sucesso!");
+  } catch (error) {
+    console.error("Erro ao criar usuário:", error);
+    res.status(500).send("Erro ao criar usuário.");
+  }
+});
+
+// Body Parser
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-//Definindo Rotas
-app.use("/login", routes);
 
-//Definindo maquina de vizualização
+// Definindo maquina de visualização
 app.set("view engine", "ejs");
 
-//Onde estão os arquivos estáticos
+// Arquivos de renderização
+app.set("views", path.join(__dirname, "/views"));
 
-//CSS
+// Usando as rotas definidas
+app.use("/", router);
+
+// Onde estão os arquivos estáticos
+// CSS
 app.use(express.static(path.join(__dirname, "/public/css")));
-//JS
+// JS
 app.use(express.static(path.join(__dirname, "/public/js")));
 
-//Arquivos de renderização:
-app.set("views", path.join(__dirname, "/views/"));
-
-
-const door = 3700;
+const door = 4900;
 app.listen(door, () => {
-    console.log(`Start Success in door! ${door}`);
+  console.log(`Servidor iniciado com sucesso na porta ${door}`);
 });
